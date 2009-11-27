db9016465f786dd3d569d5b2a1f89edf5fa29669
brad <brad@dc5f9297-5a66-0410-9128-e8096d0e5a01>
2009-11-27 14:53:49 +0000

    cleaned up

--- diags/loadtomem.c
+++ diags/loadtomem.c
@@ -14,7 +14,7 @@ main()
 	unsigned orig, count;
 
 	size = read(0, buffer, sizeof(buffer));
-	printf("size %d\n", size);
+	fprintf(stderr, "size %d\n", size);
 
 #if 0
 	for (o = 0; o < size; o++) {
@@ -77,11 +77,12 @@ main()
 
 		case 5:					/* high origin */
 			origin = (i << 8) | origin;
-			printf("origin %o, count %d, offset 0x%x\n", origin, count, o);
+			fprintf(stderr, "origin %o, count %d, offset 0x%x\n",
+				origin, count, o);
 			if (count == 6) {
 				PC = 0;
 				if (origin != 1) PC = origin & 0177776;
-				printf("PC %o\n", PC);
+				fprintf(stderr, "PC %o\n", PC);
 				goto done;
 			}
 			count = count - 6;
@@ -99,19 +100,19 @@ main()
 			break;
 
 		case 7:					/* checksum */
-			if (csum & 0377) printf("bad checksum %d\n", o);
+			if (csum & 0377)
+				fprintf(stderr, "bad checksum %d\n", o);
 			csum = state = 0;
 			break;
 		}
 	}
 
 done:
-	printf("done, pc %o\n", PC);
+	fprintf(stderr, "done, pc %o\n", PC);
 
-	printf("dump:\n");
 	for (i = 0; i < 64*1024; i += 2) {
 		if (hit[i] || hit[i+1]) {
-			printf("  0%06o, 0%06o,\n", i, M[i >> 1]);
+			printf("0%06o 0%06o\n", i, M[i >> 1]);
 		}
 	}
 #endif
