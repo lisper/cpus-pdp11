0a171da99548ae24cb488e8ec94758be9949550e
brad <brad@dc5f9297-5a66-0410-9128-e8096d0e5a01>
2009-11-27 13:43:01 +0000

    moved files out of rtl directory

--- rtl/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#cver +loadvpi=../pli/ide/pli_ide.so:vpi_compat_bootstrap run_bus.v >log
-cver +loadvpi=../pli/ide/pli_ide.so:vpi_compat_bootstrap run.v >log
-
--- rtl/run2.sh
+++
@@ -1,4 +0,0 @@
-cver +loadvpi=../pli/ide/pli_ide.so:vpi_compat_bootstrap \
-     +loadvpi=../pli/rk/pli_rk.so:vpi_compat_bootstrap \
-     +loadvpi=../pli/pdp11/pli_pdp11dis.so:vpi_compat_bootstrap \
-     run.v
